3ec3c550e3d850f44315bc6510714767d629ace7
Bruno <b-coimbra@hotmail.com>
2019-04-19 15:31:19 -0300

    added the bottom navigation bar and the widgets' placeholder

---
+++ lib/home_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'homepage_widget.dart';
+import 'search_widget.dart';
+import 'profile_widget.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomepageWidget(),
+    SearchWidget(),
+    ProfileWidget()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('My Flutter App'),
+      // ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.search),
+            title: new Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile')
+          )
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+        _currentIndex = index;
+    });
+  }
+}
